0aa0770b05d9ce2690b9d955e83d5d7a72ceaee5
公司电脑1 <1@qq.com>
2024-08-13 14:56:47 +0800

    完成数据传输

--- app/src/main/java/com/example/lc29h_rtk_app/main_fragment/bluetooth_topfragment/Bluetooth_top2Fragment.java
+++ app/src/main/java/com/example/lc29h_rtk_app/main_fragment/bluetooth_topfragment/Bluetooth_top2Fragment.java
@@ -1,14 +1,18 @@
 package com.example.lc29h_rtk_app.main_fragment.bluetooth_topfragment;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.lc29h_rtk_app.R;
+import com.example.lc29h_rtk_app.MainActivity;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +25,10 @@ public class Bluetooth_top2Fragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private Handler handler;
+    private Runnable updateDataRunnable;
+    private static final int UPDATE_INTERVAL = 1000; // 1 second
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -62,9 +70,51 @@ public class Bluetooth_top2Fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_bluetooth_top2, container, false);
+        TextView ggaTextView = view.findViewById(R.id.gga_text_view);
+        TextView rmcTextView = view.findViewById(R.id.rmc_text_view);
 
+        handler = new Handler();
+        updateDataRunnable = new Runnable() {
+            @Override
+            public void run() {
+                updateData(ggaTextView, rmcTextView);
+                handler.postDelayed(this, UPDATE_INTERVAL);
+            }
+        };
 
-
+        handler.post(updateDataRunnable);
         return view;
+    }
+    private void updateData(TextView ggaTextView, TextView rmcTextView) {
+        MainActivity activity = (MainActivity) getActivity();
+        if (activity != null) {
+            String ggaData = activity.getReadGGAString();
+            String rmcData = activity.getReadRMCString();
+            String[] ggaParts = ggaData.split(",");
+            String[] rmcParts = rmcData.split(",");
+
+            if (ggaParts.length >= 15) {
+                String ggaInfo = "GGA Data:\n" +
+                        "标识符: " + ggaParts[0] + "\n" +
+                        "时间: " + ggaParts[1] + "\n" +
+                        "纬度: " + ggaParts[2] + " " + ggaParts[3] + "\n" +
+                        "经度: " + ggaParts[4] + " " + ggaParts[5] + "\n" +
+                        "标志位: " + ggaParts[6] + "\n" +
+                        "卫星颗数: " + ggaParts[7] + "\n" +
+                        "海拔高度: " + ggaParts[9] + " " + ggaParts[10];
+                ggaTextView.setText(ggaInfo);
+            } else {
+                ggaTextView.setText("Invalid GGA data");
+            }
+
+            String rmcInfo = "RMC Data:\n" + String.join(", ", rmcParts);
+            rmcTextView.setText(rmcInfo);
+        }
+    }
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (handler != null) {
+            handler.removeCallbacks(updateDataRunnable);
+        }
     }
 }
